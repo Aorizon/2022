8541b332a107329a434022a2b17448d7e7fc9eef
JOHANN VISSER <JOHANNVISSER9@gmail.com>
2022-05-25 21:30:57 +0800

    l

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,14 +1,2 @@
 import pandas as pd
-import os
-import csv
 
-#Reading raw data
-scores = pd.read_csv("C:/repos/2022/data/raw/Scores.csv")
-pd.DataFrame.head(scores)
-
-#Data quality checks
-
-#Data cleaning
-
-#Saving cleaned data
-scores.to_csv("C:/repos/2022/data/interim/scores_cleaned.csv")
