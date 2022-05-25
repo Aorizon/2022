a98ee7f84e5577f0d8d4f784b68c34fbd32b1caf
JOHANN VISSER <JOHANNVISSER9@gmail.com>
2022-05-25 21:51:01 +0800

    check

---
+++ knowledge_base/python.py
@@ -0,0 +1,3 @@
+#Data ETL#
+#Read cleaned data
+orderlist = pd.read_csv("C:/repos/2022/data/raw/asx/orderlist.csv")
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #Read cleaned data
-scores_cleaned = pd.read_csv("C:/repos/2022/data/interim/scores_cleaned.csv")
+orderlist = pd.read_csv("C:/repos/2022/data/raw/asx/orderlist.csv")
 
 #Create aggregate summaries
 #scores_agg = 
